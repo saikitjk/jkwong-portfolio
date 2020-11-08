5395b3fc1b50cf4a2ff2639f5c5ff5f77458d1a7
JK <saikitjk@gmail.com>
2020-11-08 13:09:58 -0800

    created react app for my portfolio, added bootstrap and npm install

---
+++ jkwong-portfolio/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Home from "./components/pages/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
