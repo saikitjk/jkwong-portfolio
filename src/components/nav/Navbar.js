3c9334a083fc3eaa372cea24913eb11afd77aed8
JK <saikitjk@gmail.com>
2020-11-09 14:21:52 -0800

    added hover effect on each nav object and created useState for avtive object

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style.css";
 
 export default function Navbar() {
+  const [activeState, setActiveState] = useState("About");
   return (
     <>
       <div className="navbar">
-        <div className="navbar_active">About</div>
+        <div className="navbar_active">{activeState}</div>
 
         <div className="navbar_objects">
-          <div className="navbar_object">About</div>
-          <div className="navbar_object">Resume</div>
-          <div className="navbar_object">Projects</div>
+          <div
+            className="navbar_object"
+            onClick={() => setActiveState("About")}
+          >
+            About
+          </div>
+          <div
+            className="navbar_object"
+            onClick={() => setActiveState("Resume")}
+          >
+            Resume
+          </div>
+          <div
+            className="navbar_object"
+            onClick={() => setActiveState("Projects")}
+          >
+            Projects
+          </div>
         </div>
       </div>
     </>
