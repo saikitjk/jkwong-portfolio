a9d8c9ed2fbefc3f53f84f2dc4096a79637c542d
JK <saikitjk@gmail.com>
2020-11-09 17:01:51 -0800

    added skill set into about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,12 +1,30 @@
 import React from "react";
 import Intro from "./Intro";
+import ReactLogo from "../../assets/images/react.png";
+import Javascript from "../../assets/images/javascript.png";
 
 export default function About() {
+  const skills = [
+    {
+      skillIcon: ReactLogo,
+      skillTitle: "React",
+      skillDesc: null,
+    },
+    {
+      skillIcon: Javascript,
+      skillTitle: "Javascript",
+      skillDesc: null,
+    },
+  ];
   return (
     <div className="about">
       <h6>
         <Intro />
       </h6>
+      <div className="container myGiveList">
+        <h6 className="listTitle">Tech I use</h6>
+        <div className="row">{}</div>
+      </div>
     </div>
   );
 }
