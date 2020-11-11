8cbf47048f61f8ee2132e21d45e689fdb2ddff99
JK <saikitjk@gmail.com>
2020-11-10 18:46:31 -0800

    updated home route

--- src/components/utils/projectData.js
+++ src/components/utils/projectData.js
@@ -32,6 +32,50 @@ const projectData = [
     githubURL: "https://github.com/saikitjk/CodingQuizChallenge",
     category: ["html", "css", "Javascript", "jQuery"],
   },
+  {
+    title: "Day Planner",
+    snapShot: DayPlan,
+    desc:
+      "A day planner that helps users to plan and organize their daily plans.",
+    demoURL: "https://saikitjk.github.io/DayPlanner/main.html",
+    githubURL: "https://github.com/saikitjk/DayPlanner",
+    category: ["html", "css", "Javascript", "jQuery"],
+  },
+  {
+    title: "Weather Dashboard",
+    snapShot: WeatherDash,
+    desc:
+      "A  weather dashboard that provides current weather and forecast data by cities and user's geocode.",
+    demoURL: "https://saikitjk.github.io/WeatherDashboard/weatherDash.html",
+    githubURL: "https://github.com/saikitjk/WeatherDashboard",
+    category: ["html", "css", "Javascript", "jQuery", "API"],
+  },
+  {
+    title: "Newletter-Made-EZ",
+    snapShot: Project1,
+    desc: "A Newsletter application",
+    demoURL: "https://sheikb08.github.io/UW-Project-1/",
+    githubURL: "https://github.com/saikitjk/Newletter-Made-EZ-Project",
+    category: ["html", "css", "Javascript", "jQuery", "API"],
+  },
+  {
+    title: "Readme Generator",
+    snapShot: ResumeGen,
+    desc:
+      "A Readme generator is a CLI application that helps users quickly create a professional README for a project",
+    demoURL:
+      "https://drive.google.com/file/d/1cA9iO_rHOPvi6n-X92jT8BCtyjEPQMTV/preview",
+    githubURL: "https://github.com/saikitjk/ReadmeGenerator",
+    category: ["html", "css", "Javascript", "Nodejs"],
+  },
+  {
+    title: "Note Taker",
+    snapShot: NoteTaker,
+    desc: "A note taking application that allows use to manage their notes.",
+    demoURL: "https://notestaker-jk.herokuapp.com/",
+    githubURL: "https://github.com/saikitjk/NoteTaker",
+    category: ["html", "css", "Javascript", "Nodejs", "Expressjs", "Heroku"],
+  },
 ];
 
 export default projectData;
