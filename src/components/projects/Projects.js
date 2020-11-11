b62c065398dc997e914c44682f21141567a3dac2
JK <saikitjk@gmail.com>
2020-11-10 22:57:38 -0800

    added project filter into footer

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,21 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import ProjectData from "../utils/projectData";
 import ProjectCard from "../projects/ProjectCard";
+
 import "./style.css";
 
 export default function Projects() {
+  const [project, setProjectState] = useState(ProjectData);
+
+  const filterCategory = (categoryName) => {
+    const newDisplayProject = ProjectData.filter((project) =>
+      project.category.includes(categoryName)
+    );
+    setProjectState(newDisplayProject);
+  };
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
   return (
-    <div className="container projectContainer">
-      <div className="row projectRow">
-        {ProjectData.map((project) => (
-          <ProjectCard key={project.title} project={project} />
-        ))}
+    <>
+      <div className="container projectContainer">
+        <div className="row projectRow">
+          {project.map((project) => (
+            <ProjectCard key={project.title} project={project} />
+          ))}
+        </div>
       </div>
-    </div>
+      <div className="footerContainer">
+        <div className="row projectIndex">
+          <div onClick={() => filterCategory("")}>All</div>
+          <div onClick={() => filterCategory("React")}>React</div>
+          <div onClick={() => filterCategory("MongoDB")}>MongoDB</div>
+          <div onClick={() => filterCategory("MySQL")}>MySQL</div>
+          <div onClick={() => filterCategory("Expressjs")}>Expressjs</div>
+          <div onClick={() => filterCategory("Nodejs")}>Nodejs</div>
+        </div>
+      </div>
+    </>
   );
 }
