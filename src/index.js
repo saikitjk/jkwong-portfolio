9c292cb363bea5af78549fd50151db3d20f0c966
JK <saikitjk@gmail.com>
2020-11-30 11:51:27 -0800

    fixing the github page 404 error by updating the redirect script

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/jkwong-portfolio">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
