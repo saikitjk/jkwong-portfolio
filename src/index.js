b4236cfc541459ad9638141196651bd2af665766
JK <saikitjk@gmail.com>
2020-11-30 12:32:32 -0800

    replaced BroswerRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <BrowserRouter basename="/jkwong-portfolio">
+  <HashRouter basename="/jkwong-portfolio">
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 
